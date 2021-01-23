a3d4c16d83c9a2b961d4d9a286fbd657fd0e8fb3
Matt Hedley <matthedley@hotmail.co.uk>
2021-01-23 12:20:00 +0000

    Add alternate version of KF.

--- kalman_filter.h
+++ kalman_filter.h
@@ -129,4 +129,73 @@ class KalmanFilter {
 } // namespace kf1
 
 
+
+namespace kf2 {
+
+template<size_t N, size_t M>
+class KalmanFilter {
+public:
+    using ProcessModel = std::function<Eigen::Matrix<double, N, 1>(const Eigen::Matrix<double, N, 1>&)>;
+    using MeasurementModel = std::function<Eigen::Matrix<double, M, 1>(const Eigen::Matrix<double, N, 1>&)>;
+
+    KalmanFilter(ProcessModel process_model, MeasurementModel measurement_model)
+    : process_model_(process_model), measurement_model_(measurement_model) {
+        // Convert process model function into matrix
+        const Eigen::Matrix<double, N, N> IF = Eigen::Matrix<double, N, N>::Identity();
+        Eigen::Matrix<double, N, N> F;
+        for (size_t col_idx = 0; col_idx < N; ++col_idx) {
+            F.col(col_idx) = process_model_(IF.col(col_idx));
+        }
+        // Convert measurement model function into matrix
+        const Eigen::Matrix<double, N, N> IH = Eigen::Matrix<double, N, N>::Identity();
+        Eigen::Matrix<double, M, N> H; // measurement model as matrix
+        for (size_t col_idx = 0; col_idx < N; ++col_idx) {
+            H.col(col_idx) = measurement_model_(IH.col(col_idx));
+        }
+    };
+
+    void Predict(const Eigen::Matrix<double, N, N>& process_noise) {
+        const Eigen::Matrix<double, N, N>& Q = process_noise;
+
+        state_.x = F_ * state_.x;
+        state_.P = F_ * state_.P * F_.transpose() + Q;
+    };
+
+    void Update(const Eigen::Matrix<double, M, 1>& measurement,
+                const Eigen::Matrix<double, M, M>& measurement_noise) {
+        const Eigen::Matrix<double, M, M>& R = measurement_noise;
+        Eigen::Matrix<double, M, 1> z_hat = H_ * state_.x;
+        Eigen::Matrix<double, M, 1> y = measurement - z_hat;
+        Eigen::Matrix<double, M, M> S = H_ * state_.P * H_.transpose() + R;
+        Eigen::Matrix<double, N, M> K = state_.P * H_.transpose() * S.inverse();
+
+        state_.x = state_.x + K * y;
+        state_.P = (Eigen::Matrix<double, N, N>::Identity() - K * H_) * state_.P;
+    };
+
+    Eigen::Matrix<double, N, 1> GetState() const {
+        return state_.x;
+    };
+    Eigen::Matrix<double, N, N> GetCov() const {
+        return state_.P;
+    };
+    void SetState(const Eigen::Matrix<double, N, 1>& new_state) {
+        state_.x = new_state;
+    };
+    void SetCov(const Eigen::Matrix<double, N, N>& new_cov) {
+        state_.P = new_cov;
+    };
+
+private:
+    kf::State<N> state_;
+    ProcessModel process_model_;
+    Eigen::Matrix<double, N, N> F_; // process_model_ as matrix
+    MeasurementModel measurement_model_;
+    Eigen::Matrix<double, M, N> H_; // measurement_model_ as matrix
+};
+
+} // namespace kf2
+
+
+
 #endif //SIMPLE_UKF_KALMAN_FILTER_H
--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main() {
         std::cout << "Update: " << skf.Update(z * (double)i).transpose() << "\n\n";
     }
 
-    std::cout << "Templated kalman filter\n";
+    std::cout << "Templated kalman filter 1\n";
 
     kf1::KalmanFilter<4> kf1;
     kf1::ProcessModel<4> process = [](const Eigen::Matrix<double, 4, 1>& x) -> Eigen::Matrix<double, 4, 1> {
@@ -59,6 +59,35 @@ int main() {
         std::cout << "Update: " << kf1.GetState().transpose() << "\n\n";
     }
 
+    ////////////////////////////////////////////////////////////////////
+
+    std::cout << "Templated kalman filter 2\n";
+
+    kf2::KalmanFilter<4, 2>::ProcessModel process_model_2 = [](const Eigen::Matrix<double, 4, 1>& x) -> Eigen::Matrix<double, 4, 1> {
+        Eigen::Matrix<double, 4, 4> F = Eigen::Matrix<double, 4, 4>::Identity();
+        F(0, 2) = 0.1; // px' = px + vx*dt
+        F(1, 3) = 0.1; // py' = py + vy*dt
+        return F * x;
+    };
+    kf2::KalmanFilter<4, 2>::MeasurementModel measurement_model_2 = [](const Eigen::Matrix<double, 4, 1>& x) -> Eigen::Matrix<double, 2, 1> {
+        Eigen::Matrix<double, 2, 4> H = Eigen::Matrix<double, 2, 4>::Zero();
+        // Measure position only
+        H(0, 0) = 1.0;
+        H(1, 1) = 1.0;
+        return H * x;
+    };
+
+    kf2::KalmanFilter<4, 2> kf2(process_model_2, measurement_model_2);
+
+    for (int i = 0; i < 10; i++) {
+//        std::cout << "Iteration: " << i << '\n';
+        kf2.Predict(Q);
+//        std::cout << "Predict: " << kf2.GetState().transpose() << '\n';
+        kf2.Update(z * (double)i, R);
+//        std::cout << "Measurement: " << (z * (double)i).transpose() << "\n";
+//        std::cout << "Update: " << kf2.GetState().transpose() << "\n\n";
+    }
+    std::cout << "Update: " << kf1.GetState().transpose() << "\n\n";
 
 
     return 0;
