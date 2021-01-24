cfd55e1cc79c5df02f0f938946778a7af5dd194e
Matt Hedley <matthedley@hotmail.co.uk>
2021-01-24 19:22:18 +0000

    Removed prints.

--- standard_kf.h
+++ standard_kf.h
@@ -37,7 +37,6 @@ class KalmanFilter {
 
     state_.x = F * state_.x;
     state_.P = F * state_.P * F.transpose() + Q;
-    std::cout << state_.P << "\n\n";
   };
   template<size_t M>
   void Update(MeasurementModel<N, M> measurement_model, const Eigen::Matrix<double, M, 1>& measurement,
@@ -108,7 +107,6 @@ class KalmanFilter {
 
     state_.x = F_ * state_.x;
     state_.P = F_ * state_.P * F_.transpose() + Q;
-    std::cout << state_.P << "\n\n";
   };
 
   void Update(const Eigen::Matrix<double, M, 1>& measurement,
